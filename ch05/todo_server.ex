71b0a6fc28adb1c856578205305aed72e4efc4a7
Kosmas <kosmas@mach7x.com>
2014-12-22 20:43:24 +0000

    add entries request

--- ch05/todo_server.ex
+++ ch05/todo_server.ex
@@ -5,7 +5,17 @@ defmodule TodoServer do
 
   def add_entry(todo_server, new_entry) do
     send(todo_server, {:add_entry, new_entry})
-  endi
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self, date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after 5000 ->
+      {:error, :timeout}
+    end
+  end
 
   defp loop(todo_list) do
     new_todo_list = receive do
@@ -18,6 +28,11 @@ defmodule TodoServer do
 
   defp process_message(todo_list, {:add_entry, new_entry}) do
     TodoList.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
   end
 
 end
