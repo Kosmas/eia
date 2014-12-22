9b5d1a8c2c4209f76d2ee6edac8d677b3af850dc
Kosmas <kosmas@mach7x.com>
2014-12-22 20:37:31 +0000

    the add_entry request

--- ch05/todo_server.ex
+++ ch05/todo_server.ex
@@ -3,6 +3,10 @@ defmodule TodoServer do
     spawn(fn -> loop(TodoList.new) end)
   end
 
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  endi
+
   defp loop(todo_list) do
     new_todo_list = receive do
       message ->
@@ -10,6 +14,10 @@ defmodule TodoServer do
     end
 
     loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    TodoList.add_entry(todo_list, new_entry)
   end
 
 end
