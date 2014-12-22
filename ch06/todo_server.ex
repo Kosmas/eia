928310f6e07a4ba5aefdfaf47d6f98945d31ae4b
Kosmas <kosmas@mach7x.com>
2014-12-22 22:28:13 +0000

    implementing add_entry as a cast

--- ch06/todo_server.ex
+++ ch06/todo_server.ex
@@ -4,4 +4,9 @@ defmodule TodoServer do
   def init(_) do
     {:ok, TodoList.new}
   end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    new_state = TodoList.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
+  end
 end
