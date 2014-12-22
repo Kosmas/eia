dd44b23777fea3397a42cc83a0b924f3cac34e42
Kosmas <kosmas@mach7x.com>
2014-12-22 20:26:19 +0000

    add interface functions

--- ch05/calculator.ex
+++ ch05/calculator.ex
@@ -1,5 +1,22 @@
 defmodule Calculator do
 
+  def start do
+    spawn(fn -> loop(0) end)
+  end
+
+  def value(server_pid) do
+    send(server_pid, {:value, self})
+    receive do
+      {:response, value} ->
+        value
+    end
+  end
+
+  def add(server_pid, value), do: send(server_pid, {:add, value})
+  def sub(server_pid, value), do: send(server_pid, {:sub, value})
+  def mul(server_pid, value), do: send(server_pid, {:mul, value})
+  def div(server_pid, value), do: send(server_pid, {:div, value})
+
   defp loop(current_value) do
     new_value = receive do
       {:value, caller} ->
@@ -15,6 +32,6 @@ defmodule Calculator do
           current_value
     end
 
-    looop(new_value)
+    loop(new_value)
   end
 end
